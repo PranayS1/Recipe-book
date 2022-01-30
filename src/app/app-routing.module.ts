d5853a17b9c5fd8f46160255b5bd85003468412b
Pranay Shah <32932413+PranayS1@users.noreply.github.com>
2022-01-30 14:50:39 +0530

    Modified path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
     loadChildren: () => import('./auth/auth.module') 
                           .then(m => m.AuthModule)
   },
-  { path: '**', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: '**', redirectTo: '/recipes'},
 ];
 
 @NgModule({
