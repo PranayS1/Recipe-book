e9bf76ba67d9403e34430cd8b5b936255c2c8eff
Pranay Shah <32932413+PranayS1@users.noreply.github.com>
2022-01-30 14:55:16 +0530

    Removed Wildcard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,6 @@ const routes: Routes = [
     loadChildren: () => import('./auth/auth.module') 
                           .then(m => m.AuthModule)
   },
-  { path: '**', redirectTo: '/recipes'},
 ];
 
 @NgModule({
